e66375daee81b320db20513c24f266d5fbdb5adb
ogibaladewa <ogibaladewa@gmail.com>
2023-02-25 23:35:26 +0800

    feat: slicing sign in and sign up

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,7 +1,37 @@
-import { forwardRef, useEffect, useRef } from 'react';
+import { forwardRef, useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+
+forwardRef.propTypes = {
+    type: PropTypes.oneOf(["text", "email", "password", "number", "file"]),
+    name: PropTypes.string,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    className: PropTypes.string,
+    variant: PropTypes.oneOf(["primary", "error", "primary-outline"]),
+    autoComplete: PropTypes.string,
+    required: PropTypes.bool,
+    isFocused: PropTypes.bool,
+    handleChange: PropTypes.func,
+    placeholder: PropTypes.string,
+    isError: PropTypes.bool,
+};
 
 export default forwardRef(function TextInput(
-    { type = 'text', name, id, value, className, autoComplete, required, isFocused, handleChange },
+    {
+        type = "text",
+        name,
+        id,
+        value,
+        defaultValue,
+        variant = "primary",
+        className,
+        autoComplete,
+        required,
+        isFocused,
+        handleChange,
+        placeholder,
+        isError,
+    },
     ref
 ) {
     const input = ref ? ref : useRef();
@@ -19,14 +49,15 @@ export default forwardRef(function TextInput(
                 name={name}
                 id={id}
                 value={value}
-                className={
-                    `border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm ` +
-                    className
-                }
+                defaultValue={defaultValue}
+                className={`rounded-2xl py-[13px] px-7 w-full ${
+                    isError && "input-error"
+                } input-${variant} ${className}`}
                 ref={input}
                 autoComplete={autoComplete}
                 required={required}
                 onChange={(e) => handleChange(e)}
+                placeholder={placeholder}
             />
         </div>
     );
