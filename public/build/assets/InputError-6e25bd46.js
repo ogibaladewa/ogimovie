c86d2dc769d32254f51190f06e869536154618d1
ogibaladewa <ogibaladewa@gmail.com>
2023-05-23 22:25:29 +0800

    add build folder in public

---
+++ public/build/assets/InputError-6e25bd46.js
@@ -0,0 +1,1 @@
+import{a as e}from"./app-848fb18b.js";function a({message:r,className:t=""}){return r?e("p",{className:"text-sm text-red-600 dark:text-red-400 "+t,children:r}):null}export{a as I};
---
+++ public/build/assets/InputLabel-19ee7605.js
@@ -0,0 +1,1 @@
+import{a as l}from"./app-848fb18b.js";function s({forInput:a,value:t,className:e,children:r}){return l("label",{htmlFor:a,className:"text-base block mb-2 "+e,children:t||r})}export{s as I};
